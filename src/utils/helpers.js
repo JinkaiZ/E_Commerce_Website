27df6c0e4894415ddafe7fac93e0814bc37dc1bc
JinkaiZ <Jzha541@aucklanduni.ac.nz>
2020-12-18 20:16:22 +1300

    add sort&filter feature. add cart feature.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,14 @@
 export const formatPrice = (number) => {
-    const newNumber = Intl.NumberFormat('en-NZ', {
+    return new Intl.NumberFormat('en-US', {
         style: 'currency',
-        currency: 'NZD'
+        currency: 'USD',
     }).format(number / 100)
-    return newNumber
 }
 
-export const getUniqueValues = () => { }
+export const getUniqueValues = (data, type) => {
+    let unique = data.map((item) => item[type])
+    if (type === 'colors') {
+        unique = unique.flat()
+    }
+    return ['all', ...new Set(unique)]
+}
