ccbade09a2adf8dfaca311c731d460083415f0da
JinkaiZ <Jzha541@aucklanduni.ac.nz>
2020-12-21 10:50:49 +1300

    adjust singlePage layout

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -15,14 +15,14 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 const SingleProductPage = () => {
-  const { id } = useParams();
-  const history = useHistory();
+  const { id } = useParams()
+  const history = useHistory()
   const {
     single_product_loading: loading,
     single_product_error: error,
     single_product: product,
-    fetchSingleProduct
-  } = useProductsContext();
+    fetchSingleProduct,
+  } = useProductsContext()
 
   useEffect(() => {
     fetchSingleProduct(`${url}${id}`)
@@ -42,21 +42,31 @@ const SingleProductPage = () => {
   if (error) {
     return <Error />
   }
-  const { name, price, description, stock, stars, reviews, id: sku, company, images } = product
+  const {
+    name,
+    price,
+    description,
+    stock,
+    stars,
+    reviews,
+    id: sku,
+    company,
+    images,
+  } = product
   return (
     <Wrapper>
       <PageHero title={name} product />
       <div className='section section-center page'>
         <Link to='/products' className='btn'>
           back to products
-      </Link>
-        <div className='products-center'>
+        </Link>
+        <div className=' product-center'>
           <ProductImages images={images} />
           <section className='content'>
             <h2>{name}</h2>
-            <Stars />
-            <h5 className='price'>{formatPrice(price)}</h5>
-            <p className='desc'>{description}</p>
+            <Stars stars={stars} reviews={reviews} />
+            <h5 className='price'> {formatPrice(price)}</h5>
+            <p className='desc'> {description}</p>
             <p className='info'>
               <span>Available : </span>
               {stock > 0 ? 'In stock' : 'out of stock'}
@@ -75,7 +85,7 @@ const SingleProductPage = () => {
         </div>
       </div>
     </Wrapper>
-  );
+  )
 }
 
 const Wrapper = styled.main`
@@ -100,7 +110,6 @@ const Wrapper = styled.main`
       font-weight: 700;
     }
   }
-
   @media (min-width: 992px) {
     .product-center {
       grid-template-columns: 1fr 1fr;
